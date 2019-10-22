876406b815cef3fb203a720c19c8413e02b482b2
delvedor <tommydelved@gmail.com>
2019-10-22 12:08:32 +0200

    Removed lib from source control

--- lib/Serializer.d.ts
+++
@@ -1,7 +0,0 @@
-declare class Serializer {
-    serialize(object: any): string;
-    deserialize(json: any): any;
-    ndserialize(array: any): string;
-    qserialize(object: any): string;
-}
-export default Serializer;
--- lib/pool/index.d.ts
+++
@@ -1,4 +0,0 @@
-import BaseConnectionPool from './BaseConnectionPool';
-import ConnectionPool from './ConnectionPool';
-import CloudConnectionPool from './CloudConnectionPool';
-export { BaseConnectionPool, ConnectionPool, CloudConnectionPool };
